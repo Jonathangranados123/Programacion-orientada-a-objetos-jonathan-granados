a35bdd80da04ad487d0282c0e5f8874594001a1d
Jonathangranados123 <123481552+Jonathangranados123@users.noreply.github.com>
2023-02-07 09:24:47 -0600

    clase de conversor

---
+++ src/edu/jonathangranados123/reto3/process/Conversor.java
@@ -0,0 +1,19 @@
+package edu.jonathangranados123.reto3.process;
+
+/**
+ * esta clase contiene metodos para convertir
+ */
+
+public class Conversor {
+    /**
+     * este metodo recibe un string que representa un numero base decimal y regresa
+     * un numero en binario
+     * @param decimal numero a convertir base decimal
+     * @return numero a binario
+     */
+    public static  String convertirdecimalabinario() {
+
+
+    }
+
+}
