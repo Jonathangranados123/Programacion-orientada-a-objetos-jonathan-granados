7cf72879875b61ceb6e5dc6b57f037e47bb5c1aa
Jonathangranados123 <123481552+Jonathangranados123@users.noreply.github.com>
2023-02-11 22:49:49 -0600

    clase de conversor

---
+++ src/edu/jonathangranados123/reto3/Main.java
@@ -0,0 +1,8 @@
+package edu.jonathangranados123.reto3;
+import edu.jonathangranados123.reto3.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI.showMenu();
+    }
+}
