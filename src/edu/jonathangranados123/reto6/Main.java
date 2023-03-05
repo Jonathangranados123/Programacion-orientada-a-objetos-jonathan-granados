76325b652db0b648edf02df024c631aad2a5e617
Jonathangranados123 <123481552+Jonathangranados123@users.noreply.github.com>
2023-03-04 18:55:37 -0600

    reto5

---
+++ src/edu/jonathangranados123/reto6/Main.java
@@ -0,0 +1,12 @@
+package edu.jonathangranados123.reto6;
+
+import edu.jonathangranados123.reto6.ui.CLI;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CLI.showMenu();
+    }
+
+}
